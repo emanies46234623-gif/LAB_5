659045a467e301ee0520bab9adaa339f5356dfab
cuiatk-017 <fa23-bse-017@cuiatk.edu.pk>
2026-04-22 12:22:49 +0500

    Testing CI failure case

--- create test_main.py
+++ create test_main.py
@@ -4,7 +4,7 @@ from main import add
 class TestMain(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(2,3),50)
 
 if __name__=="__main__":
     unittest.main()
